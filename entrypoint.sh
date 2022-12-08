b8692909e362736f6b1f4656b544f809f386cbde
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-12-08 15:41:51 +0100

    entrypoint.sh: refactor the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,17 @@
 set -e
 
 echo "::group::Action context"
-echo "Running ${GITHUB_ACTION}, triggered by ${GITHUB_ACTOR}"
+echo "Running action triggered by ${GITHUB_ACTOR}"
 echo "SHA: ${GITHUB_SHA}"
-echo "Args: $*""
-echo "Workspace: "${GITHUB_WORKSPACE}"
+echo "Args: $*"
+echo "Workspace: ${GITHUB_WORKSPACE}"
+echo "cmd: ${INPUT_CMD}"
 echo
 echo "More details about available env: https://docs.github.com/en/actions/learn-github-actions/environment-variables"
 echo "More details about workflow commands: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions"
 echo
 echo "::endgroup::"
 
-bash -c "${INPUT_CMD}"
+echo "Running '$*'"
+
+bash -c "$*"
